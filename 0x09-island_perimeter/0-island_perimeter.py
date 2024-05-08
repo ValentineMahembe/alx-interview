40e82cffcde9e5633d9015af5d7026fb19c8a324
Valentine Mahembe <mahembevalentine@gmail.com>
2024-05-08 13:42:54 +0200

    correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
+"""
+Module to compute the perimeter of an island
+"""
+
+
 def island_perimeter(grid):
+    """
+    Function to compute the perimeter of the island described in grid
+
+    Args:
+        grid (list of list of int): 2D grid representing the island
+
+    Returns:
+        int: perimeter of the island
+    """
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -7,25 +22,20 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-
-                # Check left neighbor
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if left neighbor is land
-
-                # Check top neighbor
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if top neighbor is land
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
 
-# Test the function
 
-
-grid = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0]
-]
-print(island_perimeter(grid))  # Output should be 12
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
