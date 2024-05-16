a44cb370d85d8db3d293ed4dc7d7a55625a0c72a
Valentine Mahembe <mahembevalentine@gmail.com>
2024-05-16 20:20:54 +0200

    correction 1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,62 +1,78 @@
 #!/usr/bin/python3
 """
-Prime game implementation
+Prime Game
 """
+
+
+def findMultiples(num, targets):
+    """
+    Finds multiples of a given number within a list
+    """
+    for i in targets:
+        if i % num == 0:
+            targets.remove(i)
+    return targets
+
+
+def isPrime(i):
+    """
+    Check if a number is prime.
+    """
+    if i == 1:
+        return False
+    for j in range(2, i):
+        if i % j == 0:
+            return False
+    return True
+
+
+def findPrimes(n):
+    """
+    Dispatch a given set into prime numbers and non-prime numbers.
+    """
+    counter = 0
+    target = list(n)
+    for i in range(1, len(target) + 1):
+        if isPrime(i):
+            counter += 1
+            target.remove(i)
+            target = findMultiples(i, target)
+        else:
+            pass
+    return counter
 
 
 def isWinner(x, nums):
     """
-    Implementation
+    Maria and Ben are playing a game.Given a set of consecutive integers
+    starting from 1 up to and including n, they take turns choosing a
+    prime number from the set and removing that number and its
+    multiples from the set.
+    The player that cannot make a move loses the game.
+
+    They play x rounds of the game, where n may be different for each round.
+    Assuming Maria always goes first and both players play optimally,
+    determine who the winner of each game is.
     """
-    if x < 1 or not nums:
-        return None
-
-    # Maximum possible value of n
-    max_n = max(nums)
-
-    # Sieve of Eratosthenes to find all primes up to max_n
-    primes = [True] * (max_n + 1)
-    primes[0] = primes[1] = False
-    p = 2
-    while (p * p <= max_n):
-        if primes[p] is True:
-            for i in range(p * p, max_n + 1, p):
-                primes[i] = False
-        p += 1
-
-    # List of prime numbers up to max_n
-    prime_nums = [i for i, is_prime in enumerate(primes) if is_prime]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    # Function to determine the winner of a single round
-    def play_game(n):
-        # Mark primes and their multiples
-        remaining = list(range(1, n + 1))
-        turn = 0  # 0 for Maria, 1 for Ben
-
-        for prime in prime_nums:
-            if prime > n:
+    players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
                 break
-            if prime in remaining:
-                turn ^= 1
-                remaining = [x for x in remaining if x % prime != 0]
+        temp = findPrimes(cluster)
 
-        return "Maria" if turn == 1 else "Ben"
+        if temp % 2 == 0:
+            players['Ben'] += 1
+        elif temp % 2 != 0:
+            players['Maria'] += 1
 
-    # Play each game
-    for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Maria'] < players['Ben']:
+        return 'Ben'
     else:
         return None
